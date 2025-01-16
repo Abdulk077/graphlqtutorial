767a637643aaa94a4f2094ea99cb25af2dd673f9
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-01-16 22:45:56 +0530

    learn about some types and how interconnect the type and resolvers and about the type and resolver also

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,23 @@
-const {UserList} = require('../Fakedata');
+const {UserList,MoviesList} = require('../Fakedata');
+const _ = require("lodash");
 const resolvers = {
     Query: {
-        users(){
+        users:()=>{
             return UserList; 
-        }
-    }
-}
+        },
+        user: (parent,args) => {
+            const id = args.id;
+            const user = _.find(UserList, {id: Number(id)});
+            return user;
+        },
+        movies:()=>{
+            return MoviesList;
+        },
+        movie:(parent,args) => {
+            const name = args.name;
+            const movie = _.find(MoviesList, {name: name});
+            return movie;
+        },
+    },
+};
 module.exports = {resolvers};
--- schema/type-Defs.js
+++ schema/type-Defs.js
@@ -1,17 +1,26 @@
 const {gql} = require('apollo-server');
 
 const typeDefs = gql`
-    type User {
-        id: ID!
-        name: String!
-        username: String!
-        age: Int!
-        nationality: String!
-    }
-    type Query {
-    users : [User!]!
-    
-    }
+  type User {
+    id: ID!
+    name: String!
+    username: String!
+    age: Int!
+    nationality: String!
+    friends: [User]
+  }
+  type Movie {
+    id: ID!
+    name: String!
+    yearofpublication: Int!
+    isInTheater: Boolean!
+  }
+  type Query {
+    users: [User!]!
+    user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
 `;
 
 module.exports = {typeDefs};
